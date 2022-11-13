3107a3bf069a87d3ff7c6838aec93dcd4ca9c410
Gilbert M Muneka <115854608+munekamgilbert@users.noreply.github.com>
2022-11-13 15:54:56 -0600

    Add files via upload

---
+++ inheritance/tests.js
@@ -0,0 +1,19 @@
+//Gilbert Muneka Mumbere
+
+describe("filter", function () {
+    it("takes a string and return the filtered result", function () {
+        assert.equal("This house is nice!", "This house is not nice!".filter('not'));
+    });
+    it("takes a array of string and return the filtered result", function () {
+        assert.equal("house is nice!", "This house is not nice!".filter(['This','not']));
+    });
+});
+
+describe("bubbleSort", function () {
+    it("takes an empty array and return empty array", function () {
+        assert.deepEqual([], [].bubbleSort());
+    });
+    it("takes an array of 6 numbers and return bubble sorted array", function () {
+        assert.deepEqual([-3,-2, 0, 1, 3, 4,5, 6], [6,4,0, 3,-2,1,-3,5].bubbleSort());
+    });
+});
