7cf834dc9a3098f9709de6db33ef8db2d5af1b65
Gilbert M Muneka <115854608+munekamgilbert@users.noreply.github.com>
2022-11-15 07:56:11 -0600

    Add files via upload

---
+++ lab_Assignment_10/savingsaccount.js
@@ -0,0 +1,32 @@
+//Gilbert Muneka Mumbere
+"use strict";
+
+class SavingsAccount extends Account {
+    constructor(accNumber, interest){
+        super(accNumber);
+        this._interest = interest;
+    }
+
+    getInterest(){
+        return this._interest;
+    }
+
+    setInterest(interest){
+        this._interest = interest;
+    }
+
+    addInterest(){
+        const amount = this._balance * this._interest / 100;
+        super.deposit(amount);
+    }
+
+    toString(){
+        return "Account " + this._number + ": balance " + this._balance + ": interest " + this._interest + "%";
+    }
+
+    endOfMonth() {
+        const amount = this._balance * this._interest / 100;
+        this.addInterest();
+        return `Interest added ${this.constructor.name} ${this._number}: balance: ${this._balance} interest: ${amount}`;
+    }
+}
